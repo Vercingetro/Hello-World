11585e8eb84f4aed188ab73752a9557162aeb51b
Vercingetro <russ.pedersen@pcc.edu>
2017-10-23 19:01:52 -0700

    initialized project

---
+++ src/pcc/edu/cis/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package pcc.edu.cis.helloworld;
+/**
+ * Prints "Hello World" to the console
+ *
+ * @author Russ Pederse
+ * @version 10/21/2017
+ */
+public class HelloWorld
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
