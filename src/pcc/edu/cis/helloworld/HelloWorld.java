1b08eb8f0f178a2379eac70a94b506bd02e7058e
Vercingetro <russ.pedersen@pcc.edu>
2017-10-23 19:25:36 -0700

    added to the message

--- src/pcc/edu/cis/helloworld/HelloWorld.java
+++ src/pcc/edu/cis/helloworld/HelloWorld.java
@@ -2,13 +2,16 @@ package pcc.edu.cis.helloworld;
 /**
  * Prints "Hello World" to the console
  *
- * @author Russ Pederse
+ * @author Russ Pedersen
  * @version 10/21/2017
  */
 public class HelloWorld
 {
     public static void main(String [] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
+        System.out.println("Hello Git");
+        System.out.println("Good night World");
+        System.out.println("Good night Git");
     }
 }
